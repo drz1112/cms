f2a5c00f4bd2bd0cb9a55420751b6ac0de87a5a2
drz1112 <ardi.ansyaahh92@gmail.com>
2024-07-01 22:04:57 +0700

    modified:   resources/views/backend/layout/footer-js.blade.php 	modified:   resources/views/backend/layout/header.blade.php

--- resources/views/backend/layout/footer-js.blade.php
+++ resources/views/backend/layout/footer-js.blade.php
@@ -1,6 +1,7 @@
 <script src="{{asset('js/app.js')}}"></script>
-
-<script src="{{asset('vendor/datatables/js/jquery.dataTables.min.js')}}"></script>
+@if (Route::is('pages.edit') || Route::is('pages.add') || Route::is('posting.add')|| Route::is('posting.edit'))
+  <script src="{{asset('vendor/datatables/js/jquery.dataTables.min.js')}}"></script>
+@endif
 <script>
     $(document).ready(function() {
       $('[data-toggle=tooltip]').tooltip();
--- resources/views/backend/layout/header.blade.php
+++ resources/views/backend/layout/header.blade.php
@@ -11,7 +11,7 @@
     <link href="{{asset('vendor/datatables/css/jquery.dataTables.min.css')}}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
-    <link href="{{asset('vendor/datatables/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+    
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
@@ -20,7 +20,7 @@
     
 
     @if (Route::is('pages.edit') || Route::is('pages.add') || Route::is('posting.add')|| Route::is('posting.edit'))
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>  
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>  
         <style src="{{ asset('js/ckeditor5/sample/styles.css')}}"></style>
         <script src="{{ asset('js/ckeditor5/build/ckeditor.js') }}"></script>
         <style type="text/css">
